f536b50f2a1790d246f746f310b26821c929862b
trungtuyen279 <60990202+trungtuyen279@users.noreply.github.com>
2026-08-24 21:23:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
    ============================================================ */
 const CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbxJ81RJ9eNfBNZL1wgwkPQzKSsfABGp0zzHCMysY0h32SYzILkMBfkkVfJf9mEO8wIdtg/exec',
-  VERSION: '1.9.0'
+  VERSION: '1.9.1'
 };
