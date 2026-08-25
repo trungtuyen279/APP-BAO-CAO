821c9169fb84bdac86a17a1da939a96cbbd052d5
trungtuyen279 <60990202+trungtuyen279@users.noreply.github.com>
2026-08-25 23:23:36 +0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-/* ============================================================
-   CẤU HÌNH — sửa duy nhất file này khi triển khai
-   API_URL: dán URL Web App lấy được ở Bước 4 hướng dẫn triển khai
-            (dạng https://script.google.com/macros/s/AKfy..../exec)
-   ============================================================ */
-const CONFIG = {
-  API_URL: ''https://script.google.com/macros/s/AKfycbxJ81RJ9eNfBNZL1wgwkPQzKSsfABGp0zzHCMysY0h32SYzILkMBfkkVfJf9mEO8wIdtg/exec',
-  VERSION: '2.4.0'
-};
